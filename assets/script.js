660b4a69d52e2fcd6a96c42abe2e9f9aec883315
Greg P <gpfeif20@gmail.com>
2023-02-25 14:01:05 -0500

    committing

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for(var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+    }
+
+/*
+
+This is an example of an array (multiple items contained within a single variable, in this case "shapes".
+The goal is to get the Chrome console to output the names of all shapes (instead of using "console.log" command
+for each shape individually). To do this, we use a "loop" - start at "x = 0" (the first shape in the list)
+then as long as is x is less than the length of the array (condition), keep running the loop and iterate over
+over each item (x++ = x+1) aka keep adding 1 to x var until all iterations are complete
+
+var shapes = ["triangle", "square", "pentagon", "circle"];
+for(var x = 0; x < shapes.length; x++) {
+    console.log(shapes[x]);
+   }
+
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+
+*/
